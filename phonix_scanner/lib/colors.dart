c7899096ad0ed52fa6a2071ffb3d7ce91d89a892
Karel <karel.raali@gmail.com>
2026-04-04 20:29:47 +0300

    fix colors

--- phonix_scanner/lib/colors.dart
+++ phonix_scanner/lib/colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const Color font = Color(0xFFF3F3F3);
   static const Color fontHighlight = Color(0xFFDCC2FE);
 
-  static const Color buttons = Color(0xFFDCC2FE);
+  static const Color buttons = Color(0xFFFFF7E8);
 
   static const Color white = Color(0xFFFFF7E8);
   static const Color black = Color(0xff020207);
@@ -18,6 +18,6 @@ class AppColors {
   static const Color hint = Color(0x99535353);
   static const Color eventIcon = Color(0xFF6A46F5);
   static const Color green = Color(0xFF27EF8C);
-  static const Color disabledPrimaryButton = Color(0xFFD4C9FC);
+  static const Color disabledPrimaryButton = Color(0x4DFFFFFF);
   static const Color error = Color(0xFFFF6467);
 }
